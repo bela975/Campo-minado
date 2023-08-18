c678385f325ee6c00cc3dfb7cda28253f1042454
bela975 <isfsca@cesar.school>
2023-08-17 21:33:43 -0300

    reestruturando o codigo

---
+++ campominado.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Isabela Spinelli on 15/08/2023.
+//
+constexpr unsigned char pixelPorCel = 8;
+constexpr unsigned char linhas = 8;
+constexpr unsigned char colunas = 8;
+constexpr unsigned char redimTela = 8;
+
+constexpr unsigned char duracaoFrame = 16667;
